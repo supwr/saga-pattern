e1b1b4e542de599c6667ac6c41826477faa1fd91
Marcelo Filho <marcelo.rodriguespires@gmail.com>
2024-06-27 21:55:00 -0300

    Creating orders/kitchen and payment services

---
+++ cmd/api/orders/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	api := gin.Default()
+
+	api.GET("/health", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Orders OK")
+	})
+
+	api.Run(":8001")
+}
